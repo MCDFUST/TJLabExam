24ab44b918f6596f30603905cf3e863764a2f624
MCDFUST <callmetjmax@gmail.com>
2024-11-05 11:56:11 +0530

    m

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,18 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function Home() {
-  return (
-    <div>
-      <h2>Welcome to the Home Page</h2>
-      <p>This is a protected page accessible after login.</p>
-      <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
+    return (
+        <div>
+            <h2>Welcome to the Home Page</h2>
+            <p>This is a protected page accessible after login.</p>
+            <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
                 <div><img src="image1.png" alt="Image 1" /></div>
                 <div><img src="image2.png" alt="Image 2" /></div>
                 <div><img src="image3.png" alt="Image 3" /></div>
                 <div><img src="image4.png" alt="Image 4" /></div>
-        </Carousel>
-    </div>
-  );
+            </Carousel>
+        </div>
+    );
 }
 
 export default Home;
